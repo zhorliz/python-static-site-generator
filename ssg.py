74c2ca4fa9ed4d93e385a43f5eece8c6bd9c2bb6
zhorliz <zhorliz@gmail.com>
2022-10-11 14:43:00 +0200

    The Site class 1

--- ssg.py
+++ ssg.py
@@ -3,8 +3,7 @@ from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
-    config = {}
-    config.update({"source": source, "dest": dest})
+    config = {"source": source, "dest": dest}
 
     Site(**config).build()
 
